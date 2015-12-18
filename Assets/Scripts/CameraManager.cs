3c127dc3fecdaea7415947aa5b851954a8b9838c
Masaya Kazama <miyan@t-p.jp>
2015-12-19 04:26:22 +0900

    シーン整理

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using DG.Tweening;
 
 public class CameraManager : SingletonMonoBehaviour<CameraManager>{
-  Camera cam;
-  float baseSize;
+
+  [SerializeField] Transform pivot;
+  [SerializeField] Camera cam;
   GridManager gm;
+  float baseSize;
   void Awake ()
   {
     if (this != Instance) {
@@ -13,23 +15,21 @@ public class CameraManager : SingletonMonoBehaviour<CameraManager>{
       return;
     }
     DontDestroyOnLoad (this.gameObject);
+    gm = GridManager.Instance;
   }
   void Start(){
-    cam = GetComponent<Camera> ();
-    gm = GridManager.Instance;
-//    baseSize = gm.gridUnit * 1.5f;
+    //    baseSize = gm.gridUnit * 1.5f;
     baseSize = cam.fieldOfView;
   }
   public void Scale(float scale){
-//    DOTween.To (() => cam.orthographicSize, x => cam.orthographicSize = x, baseSize / scale, .2f)
+    //    DOTween.To (() => cam.orthographicSize, x => cam.orthographicSize = x, baseSize / scale, .2f)
     DOTween.To (() => cam.fieldOfView, x => cam.fieldOfView = x, Mathf.Min(140f, baseSize / scale), .2f)
       .SetEase (Ease.OutQuad);
   }
   public void movePos(IntVector2 dest){
-    transform
-      .DOLocalMove (new Vector3 (dest.x * gm.gridUnit, dest.y * gm.gridUnit -10, transform.localPosition.z), .2f)
+    cam.transform
+      .DOLocalMove (new Vector3 (dest.x * gm.gridUnit, dest.y * gm.gridUnit - 10, cam.transform.localPosition.z), .2f)
       .SetEase (Ease.OutQuad);
-
-
   }
 }
+
