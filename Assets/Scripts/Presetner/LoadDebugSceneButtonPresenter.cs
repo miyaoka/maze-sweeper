c0ba8e059593a0679283e119632e414bcff9cc80
Masaya Kazama <miyan@t-p.jp>
2016-02-07 20:22:44 +0900

    シーン遷移ボタンを一般化。タイトル曲を追加

---
+++ Assets/Scripts/Presetner/LoadDebugSceneButtonPresenter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.UI;
+
+public class LoadDebugSceneButtonPresenter : LoadSceneButtonPresenter
+{
+  protected override void Awake()
+  {
+    base.Awake();
+    DontDestroyOnLoad(transform.root.gameObject);
+    GetComponentInChildren<Text>().text = sceneName.ToString();
+  }
+}
---
+++ Assets/Scripts/Presetner/LoadSceneButtonPresenter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UniRx;
+
+[RequireComponent(typeof(Button))]
+public class LoadSceneButtonPresenter : MonoBehaviour {
+
+  [SerializeField]
+  protected SceneName sceneName;
+
+  protected virtual void Awake () {
+    var gi = GameManager.Instance;
+    var btn = GetComponent<Button>();
+    btn.OnClickAsObservable()
+      .Subscribe(_ =>
+      {
+        SceneLoader.Instance.LoadScene(sceneName);
+      })
+      .AddTo(this);
+	}
+}
