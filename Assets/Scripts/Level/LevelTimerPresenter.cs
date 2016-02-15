205e13703398f6dfe968e7d093e3a450511f0e3b
Masaya Kazama <miyan@t-p.jp>
2016-02-16 03:11:10 +0900

    タイムオーバー時にノイズを追加、残り1分からライトの光量を暗くするようにした。

--- Assets/Scripts/Level/LevelTimerPresenter.cs
+++ Assets/Scripts/Level/LevelTimerPresenter.cs
@@ -14,6 +14,9 @@ public class LevelTimerPresenter : MonoBehaviour
   Image dangerTimerImage;
   [SerializeField]
   Text dangerText;
+  [SerializeField]
+  Light directionalLight;
+  float lightMaxTime = 60f;
 
   ReactiveProperty<float> wholeTimer;
   LevelManager gm;
@@ -60,11 +63,23 @@ public class LevelTimerPresenter : MonoBehaviour
   void Start()
   {
     Tweener timerTween = null;
+    Tweener lightTween = null;
 
     var timeAmount =
     wholeTimer
       .CombineLatest(gm.LevelTimerMax, (l, r) => l / r)
       .ToReactiveProperty();
+
+    wholeTimer
+      .Select(t => Mathf.Min(lightMaxTime, t) / lightMaxTime)
+      .Subscribe(t =>
+      {
+        if (lightTween != null)
+        {
+          lightTween.Kill();
+        }
+        directionalLight.DOIntensity(t * .75f + .45f, 1f).SetEase(Ease.Linear);
+      }).AddTo(this);
 
     timerImage.fillAmount = timeAmount.Value;
 
@@ -96,6 +111,9 @@ public class LevelTimerPresenter : MonoBehaviour
         dangerTimerImage.fillAmount = 1 - v;
       })
       .AddTo(this);
+
+    Tween noiseTween = null;
+
     gm
       .DangerTimer
       .Select(d => d > 0)
@@ -103,14 +121,27 @@ public class LevelTimerPresenter : MonoBehaviour
       .Subscribe(isDanger =>
       {
         dangerText.enabled = dangerTimerImage.enabled = isDanger;
+        var noise = Camera.main.GetComponent<UnityStandardAssets.ImageEffects.NoiseAndScratches>();
+
+        if (noiseTween != null)
+        {
+          noiseTween.Kill();
+        }
 
         if (isDanger)
         {
           dangerSeq.Restart();
+          noise.enabled = true;
+          noise.grainIntensityMax = .2f;
+          noiseTween = DOTween
+            .To(() => noise.grainIntensityMax, x => noise.grainIntensityMax = x, 2f, 10f);
         }
         else
         {
           dangerSeq.Pause();
+          noiseTween = DOTween
+            .To(() => noise.grainIntensityMax, x => noise.grainIntensityMax = x, 0, 1f)
+            .OnComplete(() => noise.enabled = false);
         }
       })
       .AddTo(this);
